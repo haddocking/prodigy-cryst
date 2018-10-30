57049d13f31a186a6bf5b95d8da3f115b0c3e4fc
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2018-10-30 15:34:53 +0100

    Safe _

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 ]
 
 setup(
-    name='prodigy-cryst',
+    name='prodigy_cryst',
     version = "1.0.0",
     description=("Predicts if a protein complex is biological or crystallographic."),
     url='http://github.com/haddocking/prodigy-cryst',
@@ -28,7 +28,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'prodigy-cryst = interface_classifier:main',
+            'prodigy_cryst = interface_classifier:main',
         ]
     },
     zip_safe=True
