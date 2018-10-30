e06c2398dad2ef90ea492cc5a2e770689a7d7011
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2018-10-30 17:11:04 +0100

    Moved inside package

--- prodigy_cryst/__init__.py
+++ prodigy_cryst/__init__.py
@@ -1,6 +1,1 @@
-#!/usr/bin/env python
-#
-# This code is part of the binding affinity prediction tools distribution
-# and governed by its license.  Please see the LICENSE file that should
-# have been included as part of this package.
-#
+from .interface_classifier import *
--- setup.py
+++ setup.py
@@ -13,23 +13,22 @@ requirements = [
 setup(
     name='prodigy_cryst',
     version = "1.0.0",
-    description=("Predicts if a protein complex is biological or crystallographic."),
+    description=("Predicts if a protein complex interface is biological or crystallographic."),
     url='http://github.com/haddocking/prodigy-cryst',
     author='Computational Structural Biology Group @ Utrecht University',
     author_email='prodigy.bonvinlab@gmail.com',
     license='Apache 2.0',
-    packages=['prodigy_cryst', 'prodigy_cryst.lib'],
-    package_dir={'prodigy_cryst': 'prodigy_cryst'},
+    packages=['prodigy_cryst'],
     long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
     ],
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'prodigy_cryst = interface_classifier:main',
+            'prodigy_cryst = prodigy_cryst.interface_classifier:main',
         ]
     },
-    zip_safe=True
+    zip_safe=False
 )
