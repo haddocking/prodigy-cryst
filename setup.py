f0f6080cd118a5f44d2be83ca53f4c76b28b3056
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2018-10-30 14:53:30 +0100

    Entry point

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'prodigy-cryst = prodigy-cryst.interface_classifier:main',
+            'prodigy-cryst = interface_classifier:main',
         ]
     },
     zip_safe=True
